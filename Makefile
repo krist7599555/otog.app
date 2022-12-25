485d75c8f4622f2e9d98c38c9ad937fb106609f8
Krist Ponpairin <krist7599555@gmail.com>
2022-12-25 12:08:06 +0700

    feat: ping

---
+++ Makefile
@@ -0,0 +1,14 @@
+IMAGE_NAME = "registry.fly.io/otog-app"
+define otog_version
+	$(shell cat package.json | jq -r '.version')
+endef
+
+patchversion:
+	pnpm version patch --no-git-tag-version
+
+deploy: patchversion
+	fly deploy --remote-only --auto-confirm --image-label $(otog_version) ./
+
+
+
+.PHONY: 
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,18 +1,21 @@
 import { PrismaClient } from '@prisma/client';
-import type { Handle, HandleServerError } from '@sveltejs/kit'
+import type { Handle, HandleServerError } from '@sveltejs/kit';
 
-export const _prisma = new PrismaClient()
+export const _prisma = new PrismaClient();
+
+_prisma.$queryRaw`SELECT 1 + 1`
+  .then(ok => console.log('PING OK', ok))
+  .then(err => console.error('PING ERR', err));
 
 export const handle: Handle = async ({ event, resolve }) => {
   event.locals.prisma = _prisma;
   const response = await resolve(event);
   return response;
-}
-
+};
 
 export const handleError: HandleServerError = async ({ error, event }) => {
-  console.error(error)
+  console.error(error);
   return {
-    message: `${error}`,
+    message: `${error}`
   };
-}
+};
